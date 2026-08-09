fb978f21eacdf4db70716ba43fb357b7c20a5652
Yeeun102 <164315574+Yeeun102@users.noreply.github.com>
2026-08-09 17:43:24 +0900

    feat:회원가입 후 프로필 설정

--- app/src/main/java/com/example/colorpaper/ui/profile/ProfileSetupFragment.kt
+++ app/src/main/java/com/example/colorpaper/ui/profile/ProfileSetupFragment.kt
@@ -1,4 +1,48 @@
 package com.example.colorpaper.ui.profile
 
-class ProfileSetupFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.colorpaper.databinding.FragmentProfileSetupBinding
+
+class ProfileSetupFragment : Fragment() {
+
+    private var _binding: FragmentProfileSetupBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileSetupBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 1. '중복확인' 버튼 눌렀을 때
+        binding.btnCheckDuplicate.setOnClickListener {
+            val userId = binding.etUserid.text.toString()
+
+            if (userId.isNotBlank()) {
+                Toast.makeText(requireContext(), "사용 가능한 아이디입니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // 2. '시작하기' 버튼 눌렀을 때
+        binding.btnStart.setOnClickListener {
+            Toast.makeText(requireContext(), "환영합니다!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
